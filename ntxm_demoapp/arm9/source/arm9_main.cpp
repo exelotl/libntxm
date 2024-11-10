70610ac76303d940d38ebd1eabbb2c1e060c89d1
Adrian Siekierka <kontakt@asie.pl>
2024-11-10 08:52:35 +0100

    fix building ntxm_demoapp

--- ntxm_demoapp/arm9/source/arm9_main.cpp
+++ ntxm_demoapp/arm9/source/arm9_main.cpp
@@ -1,6 +1,6 @@
-#include <nds.h>
-#include <nds/arm9/console.h> //basic print funcionality
 #include <stdio.h>
+#include <nds.h>
+#include <filesystem.h>
 
 #include <ntxm/ntxm9.h>
 
@@ -8,49 +8,40 @@ NTXM9 *ntxm9 = 0;
 
 int main(void)
 {
-	powerOn(POWER_ALL);
-	irqInit();
-	
-	irqEnable(IRQ_VBLANK);
-	
-	videoSetMode(0);
-	videoSetModeSub(MODE_0_2D | DISPLAY_BG0_ACTIVE);
-	vramSetBankC(VRAM_C_SUB_BG);
-	
 	consoleDemoInit();
-	
-	iprintf("Demo XM player!\n\nLoading song...\n");
-	
-	fifoInit();
+	nitroFSInit(NULL);
+
+	printf("Demo XM player!\n\nLoading song...\n");
+
 	ntxm9 = new NTXM9();
-	u16 err = ntxm9->load("test.xm");
-	
+	u16 err = ntxm9->load("nitro:/test.xm");
+
 	if(err != 0)
 	{
-		iprintf(ntxm9->getError(err));
+		printf(ntxm9->getError(err));
 		while(1); // Stop execution
 	}
 	else
-		iprintf("Song loaded successfuly.\nPress A to play\nPress B to stop.\n\n");
-		
+		printf("Song loaded successfuly.\nPress A to play\nPress B to stop.\n\n");
+
 	while(1)
 	{
 		scanKeys();
 		u16 keys = keysDown();
-		
+
 		if(keys & KEY_A)
 		{
-			iprintf("Playing.\n");
+			printf("Playing.\n");
 			ntxm9->play(true);
 		}
 		else if(keys & KEY_B)
 		{
-			iprintf("Stopped.\n");
+			printf("Stopped.\n");
 			ntxm9->stop();
 		}
-		
+
 		swiWaitForVBlank();
 	}
-	
+
 	return 0;
 }
