da94773ef1a59b165ded9537e086077d1c361a53
Adrian Siekierka <kontakt@asie.pl>
2025-04-25 18:14:03 +0200

    fix building under GCC 15

--- libntxm/common/source/sample.cpp
+++ libntxm/common/source/sample.cpp
@@ -30,10 +30,10 @@
  *
  ***** END LICENSE BLOCK *****/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
 
 #include "ntxm/sample.h"
 #include "ntxm/fifocommand.h"
